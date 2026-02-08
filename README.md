228c5999d0e1e95b23d15168c2982c59039e0529
Emerson Soares <remenoscodes@gmail.com>
2026-02-08 14:15:03 -0300

    Rename to git-native-issue (command stays git issue)
    - Rename formula class: GitIssue → GitNativeIssue
    - Update repo URLs to git-native-issue
    - Update description to emphasize native data model
    - Binary names stay git-issue* for simple 'git issue' command

--- README.md
+++ README.md
@@ -5,11 +5,11 @@ Distributed issue tracking system built on Git.
 ## Installation
 
 ```bash
-brew install remenoscodes/git-issue/git-issue
+brew install remenoscodes/git-native-issue/git-issue
 ```
 
 ## About
 
-This tap provides the Homebrew formula for [git-issue](https://github.com/remenoscodes/git-issue).
+This tap provides the Homebrew formula for [git-issue](https://github.com/remenoscodes/git-native-issue).
 
 For more information, see the main repository.
--- git-native-issue.rb
+++ git-native-issue.rb
@@ -1,7 +1,7 @@
-class GitIssue < Formula
-  desc "Distributed issue tracking system built on Git"
-  homepage "https://github.com/remenoscodes/git-issue"
-  url "https://github.com/remenoscodes/git-issue/releases/download/v1.0.1/git-issue-v1.0.1.tar.gz"
+class GitNativeIssue < Formula
+  desc "Distributed issue tracking using Git's native data model"
+  homepage "https://github.com/remenoscodes/git-native-issue"
+  url "https://github.com/remenoscodes/git-native-issue/releases/download/v1.0.1/git-native-issue-v1.0.1.tar.gz"
   sha256 "0539533d62a3049d8bb87d1db91d80b8da09d29026294a6e04f4f50f1fd3b437"  # Update this after creating v1.0.1 release
   license "GPL-2.0-only"
   version "1.0.1"
